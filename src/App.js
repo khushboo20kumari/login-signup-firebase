b9f5f76937046067ce22523166dd4473c089db10
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-02-27 18:20:02 +0530

    Event handler

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import { useState } from "react"
 import { app } from "./firebase"
-import {getAuth,createUserWithEmailAndPassword} from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 const auth = getAuth(app)
 
 function App() {
 
-  const OnClickHandler = () => {
-    createUserWithEmailAndPassword(auth,)
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+
+  const onChangeHandleusername=(e)=>{
+    setEmail(e.target.value)
   }
-  
+
+  const onChangeHandlerPassword=(e)=>{
+    setPassword(e.target.value)
+  }
+
+  const onChangeHandlerEmail=(e)=>{
+    setEmail(e.target.value)
+  }
+
+
+  const OnClickHandler = () => {
+    setEmail("")
+    setUserName("")
+    setPassword("")
+    createUserWithEmailAndPassword(auth, email, password)
+  }
+
   return (
 
     <div className="App">
-      <input onChange={onChangeHandleusername}></input>
-      <input onChange={onChangeHandlerPassword}></input>
-      <input onChange={onChangeHandlerEmail}></input>
+      <input onChange={onChangeHandleusername} value={userName}></input>
+      <input onChange={onChangeHandlerPassword} value={password}></input>
+      <input onChange={onChangeHandlerEmail} value={email}></input>
       <button onClick={OnClickHandler}>save</button>
     </div>
 
