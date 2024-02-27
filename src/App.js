81100959911c897c46f3233f12a42884c4949a65
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-02-27 11:14:10 +0530

    add input tag

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import SignupPage from './Component/Index/SignupPage';
 
 import './App.css';
 function App() {
   return (
     <div className="App">
+      <SignupPage/>
       
     </div>
   );
--- src/Component/Index/SignupPage.js
+++ src/Component/Index/SignupPage.js
@@ -1,7 +1,10 @@
-function SignupPage(){
-    return(
+function SignupPage() {
+    return (
         <>
-        <p>this is Signup component</p>
+            <p>this is Signup component</p>
+            <input className="userName"></input>
+            <input className="Password"></input>
+            <input className="Email"></input>
         </>
     )
 }
