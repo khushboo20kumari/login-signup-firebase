a30070df5ec859d5ff45623269a628a623660591
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-02-27 11:33:52 +0530

    ONsubmit Event handler Defind

--- src/Component/Index/SignupPage.js
+++ src/Component/Index/SignupPage.js
@@ -3,15 +3,34 @@ import { useState } from "react";
 function SignupPage() {
 
     const [userName,setUserName]=useState("")
-    const [Password,setPassword]=useState("")
+    const [password,setPassword]=useState("")
     const [email,setEmail]=useState("")
+
+    const [data,setData]=useState([])
+
+    const OnChangeHandlerUserName=(e)=>{
+        setUserName(e.target.value)
+    }
+
+    const OnChangeHandlerUserPassword=(e)=>{
+        setPassword(e.target.value)
+    }
+
+    const OnChangeHandlerUserEmail=(e)=>{
+        setEmail(e.target.value)
+    }
     
+    const OnClickSubmit=()=>{
+        
+    }
+
     return (
         <>
             <p>this is Signup component</p>
-            <input className="userName" onChange={OnChangeHandlerUserName}></input>
-            <input className="Password" onChange={OnChangeHandlerUserPassword} ></input>
-            <input className="Email" onChange={OnChangeHandlerUserEmail}></input>
+            <input className="userName" onChange={OnChangeHandlerUserName} value={userName}></input>
+            <input className="Password" onChange={OnChangeHandlerUserPassword} value={password}></input>
+            <input className="Email" onChange={OnChangeHandlerUserEmail} value={email}></input>
+            <button onClick={OnClickSubmit}></button>
         </>
     )
 }
